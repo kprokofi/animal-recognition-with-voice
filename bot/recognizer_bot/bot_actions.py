446ab8d735caed61aafd241b8907038137768371
ArtamonovDen <brightcolors46@gmail.com>
2021-06-16 01:18:48 +0300

    Update bot

--- bot/recognizer_bot/bot_actions.py
+++ bot/recognizer_bot/bot_actions.py
@@ -1,5 +1,6 @@
 from telegram import Update
 from telegram.ext import CallbackContext
+from io import BytesIO
 
 
 def send_text(update: Update, context: CallbackContext, mes: str):
@@ -10,13 +11,10 @@ def send_voice_message(update: Update, context: CallbackContext, path: str):
     with open(path, 'rb') as f:
         context.bot.sendVoice(chat_id=update.effective_chat.id, voice=f)
 
-# def send_image(update: Update, context: CallbackContext, path: str):
-#     from io import BytesIO
-#     bio = BytesIO()
-#     bio.name = 'image.jpeg'
-#     image.save(bio, 'JPEG')
-#     bio.seek(0)
-#     bot.send_photo(chat_id, photo=bio)
 
-#     with open(path, 'rb') as f:
-#         context.bot.send_photo(f)
+def send_image(update: Update, context: CallbackContext, img):
+    bio = BytesIO()
+    bio.name = 'image.jpeg'
+    img.save(bio, 'JPEG')
+    bio.seek(0)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=bio)
